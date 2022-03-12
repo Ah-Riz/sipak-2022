6a7c95caa4b3e37a595c6e6146972b5cd572637c
ahmadrizkimaulana666 <44972911+ahmadrizkimaulana666@users.noreply.github.com>
2022-03-12 20:25:12 +0700

    a

--- application/libraries/RestController.php
+++ application/libraries/RestController.php
@@ -17,7 +17,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
  *
  * @version         4.0.0
  */
-class RestController extends CI_Controller
+class RestController extends \CI_Controller
 {
     /**
      * This defines the rest format
