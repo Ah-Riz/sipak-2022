b1772941be64a2e7d8f12666f52ab991c785a83e
ahmadrizkimaulana666 <44972911+ahmadrizkimaulana666@users.noreply.github.com>
2022-03-12 19:40:22 +0700

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
 class Welcome extends CI_Controller {
 
 	/**
