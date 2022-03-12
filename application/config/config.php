018c0c012759f63cc1e68634349248f40a6d8866
ahmadrizkimaulana666 <44972911+ahmadrizkimaulana666@users.noreply.github.com>
2022-03-12 21:38:42 +0700

    a

--- application/config/config.php
+++ application/config/config.php
@@ -530,3 +530,9 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+
+$config['base_url'] = 'your_heroku_link';
+$config['index_page'] = 'views/pages/home.php';
+$config['uri_protocol'] = 'REQUEST_URI';
+$config['sess_save_path'] = sys_get_temp_dir();
