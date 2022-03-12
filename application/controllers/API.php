498bc41436de93da34da1f8948f449e8a6a86605
ahmadrizkimaulana666 <44972911+ahmadrizkimaulana666@users.noreply.github.com>
2022-03-12 22:52:46 +0700

    a

--- application/controllers/API.php
+++ application/controllers/API.php
@@ -5,37 +5,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require '/app/vendor/chriskacerguis/codeigniter-restserver/src/RestController.php';
 use chriskacerguis\RestServer\RestController;
 
-class Api extends RestController  {
+// class Api extends RestController  {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->database();
-		if ($_POST==null){
-			var_dump(json_encode($this->db->query("SELECT * FROM makanan")->result()));
-		}else{
-			dump($kriteria);
-			var_dump($this->db->query("SELECT * FROM makanan")->result());
-		}
+// 	/**
+// 	 * Index Page for this controller.
+// 	 *
+// 	 * Maps to the following URL
+// 	 * 		http://example.com/index.php/welcome
+// 	 *	- or -
+// 	 * 		http://example.com/index.php/welcome/index
+// 	 *	- or -
+// 	 * Since this controller is set as the default controller in
+// 	 * config/routes.php, it's displayed at http://example.com/
+// 	 *
+// 	 * So any other public methods not prefixed with an underscore will
+// 	 * map to /index.php/welcome/<method_name>
+// 	 * @see https://codeigniter.com/userguide3/general/urls.html
+// 	 */
+// 	public function index()
+// 	{
+// 		$this->load->database();
+// 		if ($_POST==null){
+// 			var_dump(json_encode($this->db->query("SELECT * FROM makanan")->result()));
+// 		}else{
+// 			dump($kriteria);
+// 			var_dump($this->db->query("SELECT * FROM makanan")->result());
+// 		}
 
-		// $this->load->view('welcome_message');
-	}
+// 		// $this->load->view('welcome_message');
+// 	}
 
-	public function sipak(){
-		dump('wew');
-	}
-}
+// 	public function sipak(){
+// 		dump('wew');
+// 	}
+// }
