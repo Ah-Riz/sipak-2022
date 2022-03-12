ed65e46ce436fa34244ecf037b1fdaffda6ed094
ahmadrizkimaulana666 <44972911+ahmadrizkimaulana666@users.noreply.github.com>
2022-03-12 23:30:23 +0700

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,6 +25,7 @@ class Welcome extends CI_Controller  {
 	public function index($kriteria='')
 	{
 		var_dump($kriteria);
+		var_dump($_POST);
 		$this->load->database();
 		if ($kriteria==''){
 			echo(json_encode($this->db->query("SELECT makanan FROM makanan")->result()));
